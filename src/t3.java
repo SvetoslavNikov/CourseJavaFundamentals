4fef3bd3f1a1dd9047dcea9d332914f6e0988941
SvetoslavNikov <svetoslavnikov794@gmail.com>
2023-09-21 15:30:10 +0300

    Initial commit

---
+++ src/t3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class t3 {
+
+
+    public static int main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int f (  int b, int y){
+            int a = b * y;
+            return a;
+        }
+    }
+}
---
+++ src/t4.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class t4 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
