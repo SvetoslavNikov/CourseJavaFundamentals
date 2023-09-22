9859ad9f71b513ed2ab1ec6a803d8150ff302031
SvetoslavNikov <svetoslavnikov794@gmail.com>
2023-09-22 22:34:45 +0300

    More exercise

---
+++ src/dataTypesAndVariables/sumOfChars.java
@@ -0,0 +1,24 @@
+package dataTypesAndVariables;
+import java.util.Scanner;
+//import java.lang.Math;
+public class sumOfChars {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        String a = " ";
+        int sum = 0;
+
+        for(int i = 0; i<n ; i++){
+            a = scan.nextLine();
+            char b = a.charAt(0);
+            sum += (int) b;
+        }
+        System.out.printf("The sum equals: %d", sum);
+    }
+}
+//5
+//A
+//b
+//C
+//d
+//E
