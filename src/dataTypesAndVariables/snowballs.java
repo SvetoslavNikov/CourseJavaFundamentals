38d4a01d92abb0607667abfa7ee4e6dd7865309e
SvetoslavNikov <svetoslavnikov794@gmail.com>
2023-09-25 15:34:49 +0300

    More exercise

--- src/dataTypesAndVariables/snowballs.java
+++ src/dataTypesAndVariables/snowballs.java
@@ -1,8 +1,5 @@
 package dataTypesAndVariables;
 
-
-
-//Not FINISHED
 import java.util.Scanner;
 import java.lang.Math;
 
@@ -14,13 +11,35 @@ public class snowballs {
         int time = 0;
         int quality = 0;
         int snowball = 0;
+        int highestValue = 0;
+        int printTime = 0;
+        int printSnow = 0;
+        int printQuality = 0;
         for (int i = 0; i < N; i++) {
             snow = Integer.parseInt(scan.nextLine());
             time = Integer.parseInt(scan.nextLine());
             quality = Integer.parseInt(scan.nextLine());
             snowball = (int) Math.pow((snow / time), quality);
+            if (highestValue < snowball) {
+                highestValue = snowball;
+                printTime = time;
+                printSnow = snow;
+                printQuality = quality;
+            }
 
         }
-        System.out.printf("%d : %d = %d (%d)", snow, time, snowball, quality);
+        System.out.printf("%d : %d = %d (%d)", printSnow, printTime,
+                highestValue, printQuality);
     }
 }
+
+/*3
+10
+5
+7
+16
+4
+2
+20
+2
+2*/
