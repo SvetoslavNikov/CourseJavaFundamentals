5990a25d83c1e43091cbd2ee807705ce29b3a714
SvetoslavNikov <svetoslavnikov794@gmail.com>
2023-10-03 01:01:08 +0300

    Update kaminoFactory.java

--- src/Arrays/MoreExercise/kaminoFactory.java
+++ src/Arrays/MoreExercise/kaminoFactory.java
@@ -24,59 +24,66 @@ public class kaminoFactory {
         //we have created dna - array of arrays.
         //dna[0][1] is the second number of the first array
 
-        //find the array with longest sequence of ones
+
+        //taking mejurments
         int count[] = new int[i];
         int[] totalAmountOfOnes = new int[i];
         int[] highestcountOfTheArray = new int[i];
-        int[] nullsBeforeHighestSequence = new int[i];
-        int[] nulls = new int[i];
-        int[] bestDNA = new int[n];
+        int[] digitsBeforeHighestSequence = new int[i];
+        int[] digits = new int[i];
+        int[] bestDNA = new int[i];
 
-        for (int j = i-1; j >= 0; j--) {
+        for (int j = 0; j<i; j++) {
             for (int z = 0; z < dna[j].length; z++) {
 
                 if (1 == dna[j][z]) {
                     count[j]++;
                     totalAmountOfOnes[j]++;
-                    if (count[j] > highestcountOfTheArray[j]) {
+                    if (count[j] >= highestcountOfTheArray[j]) {
                         highestcountOfTheArray[j] = count[j];
-                        nullsBeforeHighestSequence[j] = nulls[j];
+                        digitsBeforeHighestSequence[j] = digits[j]-count[j]+1;
                     }
-                    nulls[j] = 0;
+                    else if(count[j] == highestcountOfTheArray[j]) {
+                        digits[j] = 0;
+                    } else{
+
+                    digits[j]++;}
                 } else if (0 == dna[j][z]) {
                     count[j] = 0;
-                    nulls[j]++;
+                    digits[j]++;
                 }
             }
         }
+//5
+//1!0!1!1!0
+//0!1!1!0!0
+//Clone them!
+        //deciding which is the bestDNA
+        int sample = 0;
         for(int j = i-1; j>=0; j--){
             if (j != 0) {
                 if (highestcountOfTheArray[j-1] > highestcountOfTheArray[j]) {
-                    bestDNA = dna[j-1];
+                    bestDNA = Arrays.copyOf(dna[j-1], dna[j-1].length);
+                    sample = j-1;
                 } else if (highestcountOfTheArray[j-1] == highestcountOfTheArray[j]){
-                    if(nullsBeforeHighestSequence[j-1] < nullsBeforeHighestSequence[j]){
-                        bestDNA = dna[j-1];
-                    } else if (nullsBeforeHighestSequence[j-1] == nullsBeforeHighestSequence[j]) {
+                    if(digitsBeforeHighestSequence[j-1] < digitsBeforeHighestSequence[j]){
+                        bestDNA = Arrays.copyOf(dna[j-1], dna[j-1].length);
+                        sample = j-1;
+                    } else if (digitsBeforeHighestSequence[j-1] == digitsBeforeHighestSequence[j]) {
                         if(totalAmountOfOnes[j-1]>totalAmountOfOnes[j]){
-                            bestDNA = dna[j-1];
+                            bestDNA = Arrays.copyOf(dna[j-1], dna[j-1].length);
+                            sample = j-1;
                         }
                     }
                 }
             }
         }
 
+        //print bestDNA
+        System.out.printf("Best DNA sample %d with sum: %d.%n", sample+1 , highestcountOfTheArray[sample]+1);
         for (int e : bestDNA) {
             System.out.print(e + " ");
         }
-
-
-        //finding which how many ones have the lonegest
-        /*for (int j = 1; j < i; j++) {
-            if (count[j - 1] < count[j]) {
-                count[0] = count[j];
-            }
-        }
-        System.out.println(count[0]);*/
     }
 }
 
